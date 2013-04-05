56ba0309405adc824d08d978f0cdee29056500a7
xiujie-jiang <jiangxiujie@gmail.com>
2013-04-06 00:21:59 +0800

    Update tip.py

--- tip.py
+++ tip.py
@@ -7,46 +7,56 @@ import sys
 import socket
 import re
 
-try:
-    scrpit, _addr =  sys.argv
-except ValueError:
-    print "...\n......\n\n没有获取到IP地址  退出程序!!!\n........Done!"
-    sys.exit()
 
 def net_addr():
+    #域名解析
     try:
         return socket.getaddrinfo(_addr, None)
     except Exception:
-        print '抱歉！网络或服务不可用。'
+        print '抱歉！地址解析失败。'
         sys.exit()
-
-if re.search(r'^([1-2][0-5]?[0-4]?)', _addr):
-    net_addr()
-else:
-    if re.search(r'(\w+\.)?(\w+|\d+)\.(\D){2,3}$', _addr):
-        net_addr()
-    else:
-        print '域名获取失败。'
-        sys.exit()
-
-def vul_info(vule):
-    return _ip_key["data"][vule]
 
 def St_dic(arg):
+    #类型转换.字符串转字典
     return eval(arg)
+
+def get_info(_net_addr):
+    #主函数，获取ip信息
+    url =  'http://ip.taobao.com/service/getIpInfo.php?ip=' + _net_addr
+    Hc =  httplib2.Http('.cache')
+    resp, content =  Hc.request(url, 'GET')
+
+    def vul_info(vule):
+        #对返回的IP信息作筛选
+        return _ip_key['data'][vule]
+
+    if resp['status'] == '200' or '302':
+        if St_dic(content)['code'] == 0:
+            conn =  content.decode('unicode_escape')
+            #字符编码转换
+            _ip_key =  St_dic(conn)
+            print vul_info('country'),vul_info('region'),vul_info('city'),vul_info('isp'),vul_info('ip')
+        else:
+            print St_dic(content)['data']
+    else:
+        print '抱歉！服务不可用.',resp['status']
 
 if __name__ == '__main__':
 
-    url =  "http://ip.taobao.com/service/getIpInfo.php?ip=" + net_addr()[0][4][0]
-    Hc =  httplib2.Http(".cache")
-    resp, content =  Hc.request(url, "GET")
+    try:
+        scrpit, _addr =  sys.argv
+    except ValueError:
+        print '...\n......\n\n没有获取到IP地址  退出程序!!!\n........Done!'
+        sys.exit()
+        #接受输入的查询地址
 
-    if resp["status"] == "200" or "302":
-        if St_dic(content)["code"] == 0:
-            conn =  content.decode("unicode_escape")
-            _ip_key =  St_dic(conn)
-            print vul_info("country"),vul_info("region"),vul_info("city"),vul_info("isp"),vul_info("ip")
-        else:
-            print St_dic(content)["data"]
+    if re.search(r'^([1-2][0-5]?[0-4]?).*(\d)$', _addr):
+        get_info(_addr)
+        #输入为ip地址，直接GET taobao IP库信息(taobao会做IP合法性判断)
     else:
-        print "抱歉！服务不可用.",resp["status"]
+        if re.search(r'(\w+\.)?(\w+|\d+)\.(\D){2,3}$', _addr):
+            get_info(net_addr()[0][4][0])
+            #输入为域名，进行解析，在GET
+        else:
+            print '地址获取失败。'
+            sys.exit()
